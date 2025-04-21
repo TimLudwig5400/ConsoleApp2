fe722480032c9a63876bcc2c820b75114269228a
TimLudwig5400 <tludwig@partswatch.com>
2025-04-21 10:55:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Test for Define Constants");
+            string test = "";
+#if USI
+                test = "USI";
+#elif CTC
+                test = "CTC";
+#elif PW
+                test = "Partswatch";
+#endif
+
+            Console.WriteLine(test);
+
+        }
+    }
+}
